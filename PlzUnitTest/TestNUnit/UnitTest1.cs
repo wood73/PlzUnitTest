cb823aead7276fb97368834391de862986e97f5f
Alex <700jacksons@gmail.com>
2024-10-09 17:36:41 -0400

    Initial commit

---
+++ PlzUnitTest/TestNUnit/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TestNUnit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ PlzUnitTest/TestXUnit/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace TestXUnit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
